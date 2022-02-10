6e7659244104c45ae9a041438d418e20e4d1c73d
Jang Jae Yong <jangjs2412@gmail.com>
2022-02-10 17:03:25 +0900

    Stop momentum

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
 const a=5, b=2;
+let myName="JJY";
 
 console.log(a+b);
 console.log(a*b);
 console.log(a/b);
+console.log("My name is " + myName);
+
+myName="User";
+
+console.log("Your name is "+myName);
