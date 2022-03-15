22cd1846d0856a77ea7cb4736248a602b690ad24
Jang Jae Yong <jangjs2412@gmail.com>
2022-03-15 14:07:31 +0900

    preventDefault

--- js/app.js
+++ js/app.js
@@ -6,5 +6,14 @@ function loginSubmitEvent(event){
     console.log(loginInput.value);
 }
 
+function linkClick(event){
+    event.preventDefault();
+    console.log(event);
+}
+
+const link = document.querySelector("a");
+
+
 console.log("test");
 loginForm.addEventListener("submit", loginSubmitEvent);
+link.addEventListener("click",linkClick);
