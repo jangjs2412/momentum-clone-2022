cc235bc8f4d3ed6b57cd94e63f71a4b8c182ebb6
Jang Jae Yong <jangjs2412@gmail.com>
2022-03-23 14:43:50 +0900

    Loading_username

--- js/app.js
+++ js/app.js
@@ -2,16 +2,32 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+const savedUserName = localStorage.getItem(USERNAME_KEY);
 
 function loginSubmitEvent(event){
     event.preventDefault();
-    const userName = loginInput.value
+    const userName = loginInput.value;
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    greeting.classList.remove(HIDDEN_CLASSNAME);
     console.log(userName);
-    localStorage.setItem("username", userName);
+    localStorage.setItem(USERNAME_KEY, userName);
     //greeting.innerText="Hello! "+userName;
-    greeting.innerText=`Hello! ${userName}`; //백틱(``)을 사용해서 표현가능
+    //greeting.innerText=`Hello! ${userName}`; //백틱(``)을 사용해서 표현가능
+    //greeting.classList.remove(HIDDEN_CLASSNAME);
+    paintGreetings(userName);
 }
 
-loginForm.addEventListener("submit", loginSubmitEvent);
+function paintGreetings(userName){
+    greeting.innerText=`Hello! ${userName}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+
+if(savedUserName == null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", loginSubmitEvent);
+}else{
+    paintGreetings(savedUserName);
+}
+
+
