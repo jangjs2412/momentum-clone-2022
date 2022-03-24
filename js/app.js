9c1e97a46ec862baf7e5ab77580771866d6110a1
Jang Jae Yong <jangjs2412@gmail.com>
2022-03-24 15:20:06 +0900

    Updated app.js

--- js/app.js
+++ js/app.js
@@ -7,27 +7,25 @@ const savedUserName = localStorage.getItem(USERNAME_KEY);
 
 function loginSubmitEvent(event){
     event.preventDefault();
-    const userName = loginInput.value;
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    console.log(userName);
-    localStorage.setItem(USERNAME_KEY, userName);
+    localStorage.setItem(USERNAME_KEY, loginInput.value);
     //greeting.innerText="Hello! "+userName;
     //greeting.innerText=`Hello! ${userName}`; //백틱(``)을 사용해서 표현가능
     //greeting.classList.remove(HIDDEN_CLASSNAME);
-    paintGreetings(userName);
+    paintGreetings();
 }
 
-function paintGreetings(userName){
-    greeting.innerText=`Hello! ${userName}`;
+function paintGreetings(){
+    const username = localStorage.getItem(USERNAME_KEY);
+    greeting.innerText=`Hello! ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
-
 
 if(savedUserName == null){
     loginForm.classList.remove(HIDDEN_CLASSNAME);
     loginForm.addEventListener("submit", loginSubmitEvent);
 }else{
-    paintGreetings(savedUserName);
+    paintGreetings();
 }
 
 
